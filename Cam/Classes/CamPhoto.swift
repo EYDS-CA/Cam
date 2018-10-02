b34b515929faf5db789f502398eb8799e6349fd0
amirshayegh <shayegh@me.com>
2018-10-02 14:36:45 -0700

    changed Photo variables

--- Cam/Classes/CamPhoto.swift
+++ Cam/Classes/CamPhoto.swift
@@ -12,14 +12,30 @@ import CoreLocation
 public class Photo {
     public var image: UIImage?
     public var timeStamp: CMTime?
-    public var location: CLLocation?
-    public var heading: CLHeading?
+
+    // Azimuth
+    public var magneticHeading: Double = -1
+    public var trueHeading: Double = -1
+    public var headingAccuracy: Double = -1
+
+    public var latitude: Double = -1
+    public var longitude: Double = -1
 
     public var metadata: [String : Any]
 
-    init(image: UIImage, timeStamp: CMTime, location: CLLocation?,heading: CLHeading?,metadata: [String : Any]) {
+    init(image: UIImage, timeStamp: CMTime, location: CLLocation?, heading: CLHeading?, metadata: [String : Any]) {
         self.image = image
         self.timeStamp = timeStamp
         self.metadata = metadata
+
+        if let l = location {
+            self.latitude = l.coordinate.latitude
+            self.longitude = l.coordinate.longitude
+        }
+        if let h = heading {
+            self.magneticHeading = h.magneticHeading
+            self.trueHeading = h.trueHeading
+            self.headingAccuracy = h.headingAccuracy
+        }
     }
 }
